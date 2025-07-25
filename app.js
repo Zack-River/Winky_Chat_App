c567cc63e938089a82cf6d9d0a72bb4f85eda397
Abdallah Wageeh(Zack River) <111385589+Zack-River@users.noreply.github.com>
2025-07-25 17:52:25 +0300

    fixing importing in app.js from ./route => ./routes/

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const routes = require('./route/index.route');
+const routes = require('./routes/index.route');
 const corsOptions = require('./middleware/cors.Middleware');
 
 dotenv.config();
